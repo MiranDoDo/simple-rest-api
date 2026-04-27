0ebb284b45b8211ddfbcfdc303d0e18c8a74f65f
MiranDoDo <dreadforum@atomicmail.io>
2026-04-27 10:19:11 -0400

    Delete pycache

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, HTTPException
 from sqlalchemy import text
-# from src.schemas.schemas import User, UserMail
+from src.schemas.schemas import User
 from src.models.models import users_table, metadata_object
 from src.config.config import settings
-from src.core.core import engine
+from src.core.core import sync_session, engine
 
 app = FastAPI()
 
@@ -16,10 +16,32 @@ app = FastAPI()
 )
 
 def get_users():
-    with engine.connect() as conn:
+    with sync_session() as conn:
         query = "SELECT * FROM users ORDER BY id ASC"
         result = conn.execute(text(query))
         return f"{result.all()}"
+
+@app.get(
+        "/users/{user_id}",
+        tags=["Users"],
+        summary="Get user by id"
+)
+
+
+def get_user_by_id(id: int):
+    if id <= 0:
+        raise HTTPException(status_code=404, detail="Invalid id")
+    else:
+        with sync_session() as conn:
+            check_query = f"""SELECT id FROM users WHERE id = {id}"""
+            result = conn.execute(text(check_query))
+            if result.fetchone is None:
+                raise HTTPException(status_code=404, detail="User id doesn't exist")
+            else:
+                query = text(f"""SELECT * FROM users WHERE id = {id}""")
+                result = conn.execute(query)
+                return f"{result.all()}"
+
 
 # Создать новую таблицу
 
@@ -42,7 +64,7 @@ def create_table():
 )
 
 def add_user(username: str, email: str):
-    with engine.connect() as conn:
+    with sync_session() as conn:
         query = f"""INSERT INTO users (username, email) VALUES ('{username}', '{email}')"""
         conn.execute(text(query))
         conn.commit()
@@ -60,21 +82,21 @@ def update_user(id: int, new_username: str, new_email: str):
     if id <= 0:
         raise HTTPException(status_code=404, detail="Invalid id")
     else:
-        with engine.connect() as conn:
+        with sync_session() as conn:
             check_query = text(f"""SELECT id FROM users WHERE id = {id}""")
             result = conn.execute(check_query)
             if result.fetchone() is None:
                 raise HTTPException(status_code=404, detail=f"User {id} doesn't exist")
             else:
-                query = f"""UPDATE users SET username = '{new_username}', email = '{new_email}' WHERE id = {id};"""
-                conn.execute(text(query))
+                query = text(f"""UPDATE users SET username = '{new_username}', email = '{new_email}' WHERE id = {id};""")
+                conn.execute(query)
                 conn.commit()
                 return {"OK": True, "message": f"{id} has been updated: new username - {new_username}; new email - {new_email}"}
 
 # Удалить существующего юзера
 
 @app.delete(
-    "/users/{user_delete}",
+    "/users/{user_id}",
     tags=["Users"],
     summary="Delete user by his id"
 )
@@ -84,7 +106,7 @@ def delete_user(id: int):
     if id <= 0:
         raise HTTPException(status_code=404, detail="Invalid id")
     else:
-        with engine.connect() as conn:
+        with sync_session() as conn:
             check_query = text(f"""SELECT id FROM users WHERE id = {id}""")
             result = conn.execute(check_query)
             if result.fetchone() is None:
--- src/src/schemas/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field, ConfigDict, EmailStr
-
-class User(BaseModel):
-
-    username: str = Field(max_length=32)
-    email: EmailStr
-
-    model_config = ConfigDict(extra="forbid")
