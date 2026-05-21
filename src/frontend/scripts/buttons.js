f6aa3a812d7af0f62cb77bdd6392c2515dec185a
MiranDoDo <dreadforum@atomicmail.io>
2026-05-21 06:02:24 -0400

    Add More Beauty in the Interface🐛

--- src/frontend/scripts/buttons.js
+++ src/frontend/scripts/buttons.js
@@ -10,17 +10,22 @@ function button_id() {
     input_div = document.createElement("div")
     input_div.setAttribute("id", "input_div")
 
-    button_fetch = document.createElement("button")
-    button_fetch_text = document.createTextNode("post")
-    button_fetch.appendChild(button_fetch_text)
-    button_fetch.setAttribute("onclick", "get_user_by_id()")
+    input_br = document.createElement("br")
 
+    // Текст пояснения, для чего нужно данное поле ввода(Id)
     const input_quest = document.createTextNode("Enter user id")
 
     input_pole = document.createElement("input")
     input_pole.setAttribute("id", "id_user")
 
+    // Кнопка отправки данных
+    button_fetch = document.createElement("button")
+    button_fetch_text = document.createTextNode("post")
+    button_fetch.appendChild(button_fetch_text)
+    button_fetch.setAttribute("onclick", "get_user_by_id()")
+
     input_div.appendChild(input_quest)
+    input_div.appendChild(input_br)
     input_div.appendChild(input_pole)
     input_div.appendChild(button_fetch)
 
@@ -39,17 +44,22 @@ function button_delete_id() {
     input_div = document.createElement("div")
     input_div.setAttribute("id", "input_div")
 
-    button_fetch = document.createElement("button")
-    button_fetch_text = document.createTextNode("post")
-    button_fetch.appendChild(button_fetch_text)
-    button_fetch.setAttribute("onclick", "delete_user()")
+    input_br = document.createElement("br")
 
+    // Текст пояснения, для чего нужна данное поле ввода(Id)
     const input_quest = document.createTextNode("Enter user id")
 
     input_pole = document.createElement("input")
     input_pole.setAttribute("id", "id_user")
 
+    // Кнопка отправки данных
+    button_fetch = document.createElement("button")
+    button_fetch_text = document.createTextNode("post")
+    button_fetch.appendChild(button_fetch_text)
+    button_fetch.setAttribute("onclick", "delete_user()")
+
     input_div.appendChild(input_quest)
+    input_div.appendChild(input_br)
     input_div.appendChild(input_pole)
     input_div.appendChild(button_fetch)
 
@@ -68,25 +78,37 @@ function button_add_user() {
     input_div = document.createElement("div")
     input_div.setAttribute("id", "input_div")
 
-    button_fetch = document.createElement("button")
-    button_fetch_text = document.createTextNode("post")
-    button_fetch.appendChild(button_fetch_text)
-    button_fetch.setAttribute("onclick", "add_user()")
+    input_br1 = document.createElement("br")
+    input_br2 = document.createElement("br")
+    input_br3 = document.createElement("br")
+    input_br4 = document.createElement("br")
 
+    // Текст пояснения, для чего нужна данное поле ввода(Username)
     const input_username = document.createTextNode("Enter username")
 
     input_pole1 = document.createElement("input")
     input_pole1.setAttribute("id", "username")
 
+    // Текст пояснения, для чего нужна данное поле ввода(Email)
     const input_email = document.createTextNode("Enter email")
 
     input_pole2 = document.createElement("input")
     input_pole2.setAttribute("id", "email")
 
+    // Кнопка отправки данных
+    button_fetch = document.createElement("button")
+    button_fetch_text = document.createTextNode("post")
+    button_fetch.appendChild(button_fetch_text)
+    button_fetch.setAttribute("onclick", "add_user()")
+
     input_div.appendChild(input_username)
+    input_div.appendChild(input_br1)
     input_div.appendChild(input_pole1)
+    input_div.appendChild(input_br2)
     input_div.appendChild(input_email)
+    input_div.appendChild(input_br3)
     input_div.appendChild(input_pole2)
+    input_div.appendChild(input_br4)
     input_div.appendChild(button_fetch)
 
     document.body.insertBefore(input_div, actions)
@@ -104,32 +126,50 @@ function button_update_user() {
     input_div = document.createElement("div")
     input_div.setAttribute("id", "input_div")
 
-    button_fetch = document.createElement("button")
-    button_fetch_text = document.createTextNode("post")
-    button_fetch.appendChild(button_fetch_text)
-    button_fetch.setAttribute("onclick", "update_user()")
+    input_br1 = document.createElement("br")
+    input_br2 = document.createElement("br")
+    input_br3 = document.createElement("br")
+    input_br4 = document.createElement("br")
+    input_br5 = document.createElement("br")
+    input_br6 = document.createElement("br")
 
+    // Текст пояснения, для чего нужна данное поле ввода(Id)
     const input_id = document.createTextNode("Enter user id")
 
     input_pole1 = document.createElement("input")
     input_pole1.setAttribute("id", "id_user")
 
+    // Текст пояснения, для чего нужна данное поле ввода(Username)
     const input_username = document.createTextNode("Enter username")
 
     input_pole2 = document.createElement("input")
     input_pole2.setAttribute("id", "username")
 
+    // Текст пояснения, для чего нужна данное поле ввода(Email)
     const input_email = document.createTextNode("Enter email")
 
     input_pole3 = document.createElement("input")
     input_pole3.setAttribute("id", "email")
 
+    // Кнопка отправки данных
+    button_fetch = document.createElement("button")
+    button_fetch_text = document.createTextNode("post")
+    button_fetch.appendChild(button_fetch_text)
+    button_fetch.setAttribute("onclick", "update_user()")
+
+
     input_div.appendChild(input_id)
+    input_div.appendChild(input_br1)
     input_div.appendChild(input_pole1)
+    input_div.appendChild(input_br2)
     input_div.appendChild(input_username)
+    input_div.appendChild(input_br3)
     input_div.appendChild(input_pole2)
+    input_div.appendChild(input_br4)
     input_div.appendChild(input_email)
+    input_div.appendChild(input_br5)
     input_div.appendChild(input_pole3)
+    input_div.appendChild(input_br6)
     input_div.appendChild(button_fetch)
 
     document.body.insertBefore(input_div, actions)
